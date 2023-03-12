1825db5c268fae814f03bcf553c70914972f8623
Jagat Naskar <jmejagatnaskar1999@gmail.com>
2023-03-12 12:56:52 +0530

    All files added from basics_of_java assignment

---
+++ prac.java
@@ -0,0 +1,19 @@
+package Assignment1;
+
+class Studen{
+	int id = 1;
+	String name = "jaga";
+}
+public class prac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Studen s1 = new Studen();
+		System.out.println(s1.id);
+		System.out.println(s1.name);
+		
+		Studen s2 = new Studen();
+		System.out.println(s2.id);
+		System.out.println(s2.name);
+	}
+
